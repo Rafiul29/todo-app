72e15a3b05fd1ee21a9748d7410d6033c62ba907
Rafiul29 <rafiulislam4472@gmail.com>
2022-11-30 20:57:50 +0600

    todo app initialize

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import AddTask from './components/AddTask';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import TaskList from './components/TaskList';
 
-function App() {
+ const App = () => {
+const [task,setTasks]=useState([])
+
+useEffect(()=>{
+  fetchingData()
+},[])
+
+const fetchingData=async()=>{
+   try{
+    const res= await fetch("https://aluminum-delicate-snowshoe.glitch.me/tasks")
+    if(!res.ok) throw new Error("Something went wrong")
+    const data=await res.json();
+    setTasks(data)
+    console.log(data)
+   }catch(error){
+    console.log(error.message)
+   }
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper bg-gradient-to-t from-gray-900 to-teal-900 min-h-screen text-xl text-gray-100 flex flex-col py-10'>
+      <Header/>
+      <AddTask/>
+      <TaskList tasks={task} />
+      <Footer/>
     </div>
-  );
+  )
 }
 
 export default App;
